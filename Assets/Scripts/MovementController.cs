dec59fe98179f1eb5fa2794d65575219b77508c9
Kaan Dalkıran <72474584+Keaton296@users.noreply.github.com>
2024-05-20 22:09:34 +0300

    Final Touch v2

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     [SerializeField] private Transform cameraPointTransform;
+    [SerializeField] Animator armsAnimator;  
     private Rigidbody rb;
     [SerializeField] float moveMaxSpeed = 1f;
     [SerializeField] float moveAcceleration = 1f;
@@ -29,10 +30,12 @@ public class MovementController : MonoBehaviour
         if (rb.velocity.magnitude < moveMaxSpeed && !isRunning)
         {
             rb.AddForce(movementVector * moveAcceleration);
+            armsAnimator.SetBool("IsRunning", false);
         }
         else if(rb.velocity.magnitude < runMaxSpeed && isRunning)
         {
             rb.AddForce(movementVector * (moveAcceleration + runAcceleration));
+            armsAnimator.SetBool("IsRunning", true);
         }
     }
 }
